951c7058da9d3eb64fc349f1a0c5bade88a76178
David Fradel <david.fradel@gmail.com>
2024-07-14 18:30:28 +0200

    add etherscan argument in hardhat.config

--- blockchain/hardhat.config.cjs
+++ blockchain/hardhat.config.cjs
@@ -19,22 +19,25 @@ if(!process.env.PRIVATE_KEY_ADMIN) {
 const config = {
   solidity: "0.8.24",
     networks: {
-    hardhat: {},
-    localhost: {
-      url: "http://127.0.0.1:8545",
+      hardhat: {},
+      localhost: {
+        url: "http://127.0.0.1:8545",
+      },
+      sepolia: {
+        url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+        accounts: [process.env.PRIVATE_KEY_ADMIN],
+      },
+      arbitrumSepolia: {
+        url: `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+        accounts: [process.env.PRIVATE_KEY_ADMIN],
+        chainId: 421614, // Corrected Chain ID for Arbitrum Sepolia
+        gas: "auto",
+        gasPrice: "auto",
+      },
     },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.PRIVATE_KEY_ADMIN],
+    etherscan: {
+      apiKey: process.env.ETHERSCAN_API_KEY,
     },
-    arbitrumSepolia: {
-      url: `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.PRIVATE_KEY_ADMIN],
-      chainId: 421614, // Corrected Chain ID for Arbitrum Sepolia
-      gas: "auto",
-      gasPrice: "auto",
-    },
-  },
 };
 
 module.exports = config;
